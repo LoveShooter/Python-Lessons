01e99ca8c4fdd71e29aa73fc4f9cfe2c4fdfd6b8
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-03-16 01:53:11 +0300

    ok

---
+++ L7-loops.py
@@ -0,0 +1,10 @@
+for x in range(0, 100 + 1):
+    print (x)
+    print ("****")
+    print ("---------")
+
+for x in range(10, 100, 2):  ## step 2
+    print (x)
+    print ("Number x = " + str(x))   ## x = *
+    if x == 50:
+        break  ## break on 50
