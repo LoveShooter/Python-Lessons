dc863a1c32a257b90a2ed82227254de62236eb90
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-03-16 01:57:17 +0300

    ok

--- L7-loops.py
+++ L7-loops.py
@@ -8,3 +8,13 @@ for x in range(10, 100, 2):  ## step 2
     print ("Number x = " + str(x))   ## x = *
     if x == 50:
         break  ## break on 50
+
+print ("---------EndOfFile--------")
+
+
+x = 1
+while True:
+    print (x)
+    x = x + 1    ## бесконечно идет счет
+    if x == 100: 
+        break    ## break if 99 (счет с 0)
