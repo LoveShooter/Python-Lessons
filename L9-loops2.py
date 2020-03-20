ec297fa8c9992226205618eeaa7812b21ede10e8
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-03-21 01:39:42 +0300

    ok

--- L9-loops2.py
+++ L9-loops2.py
@@ -1,3 +1,4 @@
+##________0______1_____2________3________4_____
 cars = ['bmw', 'vw', 'seat', 'skoda', 'lada']
 
 for x in cars:
@@ -17,3 +18,27 @@ for x in mynumber_list:
 
 mynumber_list.sort(reverse=True)
 print(mynumber_list)
+
+print("Max number is: " + str(max(mynumber_list)))
+print("Min number is: " + str(min(mynumber_list)))
+print("Sum of list is: " + str(sum(mynumber_list)))
+
+
+
+##________0______1_____2________3________4_____
+cars = ['bmw', 'vw', 'seat', 'skoda', 'lada']
+
+mycars = cars[1:4]
+print(mycars)
+
+mycars = cars [:4]
+print(mycars)
+
+mycars = cars [-3:-1]
+print (mycars)
+
+
+##________0______1_____2________3________4_____
+cars = ['bmw', 'vw', 'seat', 'skoda', 'lada']
+mycars = cars [:]
+print(mycars)
