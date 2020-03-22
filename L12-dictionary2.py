2d794b86faa6d6e70e416c829662f370503c059a
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-03-22 13:47:41 +0300

    miss 2 lessons

---
+++ L12-dictionary2.py
@@ -0,0 +1,12 @@
+#       (-----Item-----)
+#       (key)    (value)
+
+enemy = {
+        'loc_x': 70,
+        'loc_y': 50,
+        'color': 'green',
+        'health': 100,
+        'name': 'trololo'
+
+}
+
