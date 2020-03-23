27965f64332c4d249c433dd5b33328a3e020d477
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-03-23 22:56:36 +0300

    ok

--- L12-dictionary2.py
+++ L12-dictionary2.py
@@ -6,7 +6,26 @@ enemy = {
         'loc_y': 50,
         'color': 'green',
         'health': 100,
-        'name': 'trololo'
+        'name': 'trololo',
+        'image': ['image1.jpg', 'image2.jpg', 'image3.jpg']
 
 }
 
+all_enemies = []   #все враги и равны нашему массиву
+
+for x in range(0, 10):                    #add 10 new enemies
+        all_enemies.append(enemy.copy())  #copy enemy from dictionary
+
+for ene in all_enemies:
+        print(ene)
+
+all_enemies[5]['health'] = 30             #-30 health  = 5 enemy
+all_enemies[8]['name'] = "asshole"
+all_enemies[2]['loc_x'] += 10
+
+
+print("-------------------------------")
+
+
+for ene in all_enemies:
+        print(ene)
