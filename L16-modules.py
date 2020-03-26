6ec7d754b7f960b93e9c9a99296e9643174788a4
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-03-26 16:19:58 +0300

    done

---
+++ L16-modules.py
@@ -0,0 +1,15 @@
+import L16mod  #import L16mod.py file
+
+
+
+# ----# ---------------------MAIN------------------------
+L16mod.bbb()   #import functions from L16mod.py file for bbb
+L16mod.ccc()
+L16mod.aaa()
+L16mod.ddd()
+L16mod.aaa()
+
+
+from L16mod import*   #чтобы не писать кучу строчек, можно так сократить.
+bbb()
+aaa()
