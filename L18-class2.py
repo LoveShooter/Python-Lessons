8e4d6467c1267c3e6f78a5968a58fcdf454e832f
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-03-27 23:58:16 +0300

    done

---
+++ L18-class2.py
@@ -0,0 +1,14 @@
+from hero import*
+
+myhero = Hero("LoL", 5, "Elf")
+mysuperhero = SuperHero("Devil", 99, "Human", 5)
+
+myhero.show_hero()
+mysuperhero.show_hero()
+
+mysuperhero.makemagic()
+mysuperhero.show_hero()
+
+mysuperhero.makemagic()
+mysuperhero.makemagic()
+mysuperhero.show_hero()
--- hero.py
+++ hero.py
@@ -9,7 +9,7 @@ class Hero():
 
     def show_hero(self):
         """Print all parametrs of this Hero"""
-        discription = (self.name  + " Level is: " + str(self.level) + " Race is: " + self.race + " Health is " + str(self.health)).title()
+        discription = (self.name  + " Level is: " + str(self.level) + " Race is: " + self.race + " Health is: " + str(self.health)).title()
         print(discription)
 
     def level_up(self):
@@ -22,3 +22,22 @@ class Hero():
 
     def set_health(self, new_health):
         self.health = new_health
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+
+class SuperHero(Hero):
+    """Class to Create SuperHero"""
+    def __init__(self, name, level, race, magiclevel):
+        """Initiate our Super Hero"""
+        super().__init__(name, level, race)
+        self.magiclevel = magiclevel
+        self.magic = 100
+
+    def makemagic(self):
+        """Use magic"""
+        self.magic -=10
+
+    def show_hero(self):
+        discription = (self.name  + " Level is: " + str(self.level) + " Race is: " + self.race + " Health is: " + str(self.health) + 
+        " Magic is: " + str(self.magic)).title()
+        print(discription)
